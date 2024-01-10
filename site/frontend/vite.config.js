17fc104114dbbcffcf9db3e435177ffa8e1d611a
ApurvChaturOfficial <apurvchaturoffiial@gmail.com>
2024-01-10 11:54:35 +0530

    Swayam

--- site/frontend/vite.config.js
+++ site/frontend/vite.config.js
@@ -8,16 +8,26 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      manifest:  {
-        "name": "Abol",
-        "icons": [
+      manifest: {
+        "registerType": 'autoUpdate',
+        "name":"Abol",
+        "short_name":"Abol",
+        "display": "standalone",
+        "background_color": "#072227",
+        "theme_color": "#072227",
+        "icons":[
           {
             src: "Business/Abol/favicon_io/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
-          }
+          },
+          {
+            src: "Business/Abol/favicon_io/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
         ],
-      } 
+      }
     })
   ],
  
